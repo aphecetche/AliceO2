00a435e7105b2cc0904d7926f605473939c0d2cf
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2019-10-13 22:22:21 +0200

    WIP - Add doxygen documentation for encoding parts.
    Remaining to be done : decoding parts.

--- Detectors/MUON/MCH/Raw/include/MCHRaw/SampaCluster.h
+++ Detectors/MUON/MCH/Raw/include/MCHRaw/SampaCluster.h
@@ -22,20 +22,49 @@ namespace mch
 namespace raw
 {
 
+/// @brief Piece of data for one Sampa channel.
+///
+/// A SampaCluster holds a parf of the data of one Sampa time window for
+/// one Sampa channel. This data can be in one of two forms :
+///
+/// - raw ADC samples (10 bits each)
+/// - clusterSum values (20 bits each)
+///
+/// with an associated (local) timetamp.
+///
+/// A full time window may contains several SampaClusters.
+
 struct SampaCluster {
 
+  /// Constructs a cluster which holds only a charge sum (aka cluster sum)
+  /// \param timestamp must fit within 10 bits
+  /// \param chargeSum must fit within 20 bits
+  ///
+  /// if some parameter does not fit within its expected range
+  /// a std::invalid_argument exception is thrown.
   explicit SampaCluster(uint16_t timestamp, uint32_t chargeSum);
+
+  /// Constructs a cluster which holds a vector of raw samples
+  /// \param timestamp must fit within 10 bits
+  /// \param samples : each sample must fit within 10 bits
+  ///
+  /// if some parameter does not fit within its expected range
+  /// a std::invalid_argument exception is thrown.
   SampaCluster(uint16_t timestamp, const std::vector<uint16_t>& samples);
 
+  /// nofSamples gives the number of samples of this cluster.
   uint16_t nofSamples() const;
 
+  /// isClusterSum returns 1 if this cluster is not holding raw samples.
   bool isClusterSum() const;
 
+  /// nof10BitWords returns the number of 10 bits words
+  /// needed to store this cluster
   uint16_t nof10BitWords() const;
 
-  uint16_t timestamp;
-  uint32_t chargeSum;
-  std::vector<uint16_t> samples;
+  uint16_t timestamp;            //< 10 bits for a local timestamp
+  uint32_t chargeSum;            //< 20 bits for a cluster sum
+  std::vector<uint16_t> samples; //< 10 bits for each sample
 };
 
 std::ostream& operator<<(std::ostream& os, const SampaCluster& sc);
