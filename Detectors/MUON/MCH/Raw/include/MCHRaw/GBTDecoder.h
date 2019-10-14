8cdf822199460365203889160b10a675015a9906
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2019-10-14 18:18:27 +0200

    WIP - add decoding doxygen documentation.

--- Detectors/MUON/MCH/Raw/include/MCHRaw/GBTDecoder.h
+++ Detectors/MUON/MCH/Raw/include/MCHRaw/GBTDecoder.h
@@ -24,19 +24,43 @@ namespace mch
 namespace raw
 {
 
+/// @brief A GBTDecoder groups 40 ElinkDecoder objects.
+
 class GBTDecoder
 {
  public:
+  /// Constructor.
+  /// \param cruId the identifier for the CRU this GBT is part of
+  /// \param sampaChannelHandler the callable that will handle each SampaCluster
+  /// \param chargeSumMode whether the Sampa is in clusterMode or not
   GBTDecoder(int cruId, int gbtId, SampaChannelHandler sampaChannelHandler,
              bool chargeSumMode = true);
 
-  void append(uint32_t w0, uint32_t w1, uint32_t w2, uint32_t w3);
+  /** @name Main interface 
+    */
+  ///@{
 
+  /** @brief Append one GBT word (128 bits, split in 4 32 bits words).
+    *
+    * Note that for mch data only 80 out of the 128 are of real interest.
+    */
+  void append(uint32_t w0, uint32_t w1, uint32_t w2, uint32_t w3);
+  ///@}
+
+  /** @name Methods for testing
+    */
+
+  ///@{
+
+  /// printStatus shows the status of some elinks (all if maxelink=-1)
   void printStatus(int maxelink = -1) const;
 
+  /// Ensure any leftover data is properly decoded.
   void finalize();
 
+  /// Clear our internal Elinks
   void reset();
+  ///@}
 
  private:
   void append(uint128_t w);
