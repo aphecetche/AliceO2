05c77ae3e4c6b1e621c8bc76ae351361d03e9c68
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2019-11-13 22:47:39 +0100

    WIP - TODO

---
+++ Detectors/MUON/MCH/Raw/exe/gentimeframe.cxx
@@ -0,0 +1,43 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+// gentimeframe
+//
+// helper program to generate raw data for (the equivalent of) a full
+// (sub)timeframe (parameter here is a number of DEs for which we generate
+// data)
+//
+// first generate (or get from somewhere) enough digits to "simulate"
+// the occupancy of that (sub)timeframe
+//
+// then convert all those digits into raw data buffer(s)
+// (either bare or userlogic format)
+//
+// write those buffers to disk
+//
+// have a look at the output sizes and, more importantly, at how long it takes
+// to generate them.
+//
+// have also a look at the time it takes to read back those raw data files
+//
+//
+//
+//
+// for the digit generation part, parameters :
+//
+// - number of events
+// - mean number of digits per event (-> Poisson distrib ? simple gauss ?)
+//
+// - (not needed for this project, but might come handy) : mean time between
+// two events (or some kind of event time distribution, to be able to simulate
+// e.g. pile-up or ...). That part could take as input some vectors of digits
+// and (re-)organize them into time buckets depending on some parameters.
+//
+//
