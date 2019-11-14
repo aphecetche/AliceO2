8f0918b9bdea664e90a531f9f3c75c7de3bde200
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2019-11-14 16:00:49 +0100

    WIP - gentimeframe.cxx

--- Detectors/MUON/MCH/Raw/exe/gentimeframe.cxx
+++ Detectors/MUON/MCH/Raw/exe/gentimeframe.cxx
@@ -39,5 +39,161 @@
 // two events (or some kind of event time distribution, to be able to simulate
 // e.g. pile-up or ...). That part could take as input some vectors of digits
 // and (re-)organize them into time buckets depending on some parameters.
+// => use InteractionSampler for that ?
 //
 //
+
+#include "CommonDataFormat/InteractionRecord.h"
+#include "MCHMappingInterface/Segmentation.h"
+#include "MCHSimulation/Digit.h"
+#include "MCHRawEncoder/Encoder.h"
+#include "MCHRawCommon/DataFormats.h"
+#include "MCHRawCommon/SampaCluster.h"
+#include "Steer/InteractionSampler.h"
+#include <array>
+#include <fmt/format.h>
+#include <iostream>
+#include <gsl/span>
+#include <random>
+#include <vector>
+
+using namespace o2::mch;
+
+using MCHDigit = Digit;
+
+std::vector<o2::InteractionTimeRecord> getCollisions(o2::steer::InteractionSampler& sampler,
+                                                     int nofCollisions)
+{
+  std::vector<o2::InteractionTimeRecord> records;
+  records.reserve(nofCollisions);
+  sampler.init();
+  sampler.generateCollisionTimes(records);
+  return records;
+}
+
+// generate n digits randomly distributed over the detection elements
+// whose ids are within the deids span
+std::vector<MCHDigit> generateDigits(int n, gsl::span<int> deids, gsl::span<int> nofpads)
+{
+  std::vector<MCHDigit> digits;
+  static std::random_device rd;
+  static std::mt19937 gen(rd());
+  std::uniform_int_distribution<> disDeId(0, deids.size() - 1);
+  // std::uniform_int_distribution<> disADC(0, 1023);
+
+  assert(deids.size() == nofpads.size());
+  for (auto i = 0; i < n; i++) {
+    int j = disDeId(gen);
+    int deid = deids[j];
+    std::uniform_int_distribution<> np(0, nofpads[j] - 1);
+    int padid = np(gen);
+    // double adc = disADC(gen);
+    double adc = padid;
+    double time = 0;
+    digits.emplace_back(time, deid, padid, adc);
+  }
+  return digits;
+}
+
+std::vector<int> getDetectionElementIds()
+{
+  std::vector<int> deids;
+  mapping::forEachDetectionElement([&](int deid) {
+    deids.push_back(deid);
+  });
+  return deids;
+}
+
+std::vector<std::vector<MCHDigit>> getDigits(int nofEvents, gsl::span<int> deids, float occupancy)
+{
+  std::vector<std::vector<MCHDigit>> digitsPerEvent;
+  digitsPerEvent.reserve(nofEvents); // one vector of digits per event
+  std::random_device rd;
+  std::mt19937 gen(rd());
+
+  std::vector<int> nofPads;
+  int totalPads{0};
+  for (auto d : deids) {
+    mapping::Segmentation seg(d);
+    nofPads.emplace_back(seg.nofPads());
+    totalPads += seg.nofPads();
+  }
+
+  std::poisson_distribution<> dis(static_cast<int>(occupancy * totalPads));
+
+  for (auto i = 0; i < nofEvents; i++) {
+    // draw n from mu
+    int n = static_cast<int>(dis(gen));
+    // generate n random digits
+    auto digits = generateDigits(n, deids, gsl::make_span(nofPads));
+    // add those n digits into this event
+    digitsPerEvent.push_back(digits);
+  }
+  return digitsPerEvent;
+}
+
+std::ostream& operator<<(std::ostream& os, const MCHDigit& d)
+{
+  os << fmt::format("DE {:4d} PAD {:6d} ADC {:g}\n",
+                    d.getDetID(), d.getPadID(), d.getADC());
+  return os;
+}
+
+void encode(gsl::span<o2::InteractionTimeRecord> interactions,
+            gsl::span<std::vector<MCHDigit>> digitsPerInteraction,
+            std::vector<uint8_t>& buffer)
+{
+  // for (auto i = 0; i < interactions.size(); i++) {
+  //   std::cout << interactions[i] << " ";
+  //   std::cout << " ndigits=" << digitsPerInteraction[i].size() << "\n";
+  //   // for (auto d : digitsPerEvent[i]) {
+  //   //   std::cout << d << " ";
+  //   // }
+  //   // std::cout << "\n";
+  // }
+
+  uint8_t cruId{0}; // FIXME: get this from digit (deid,padid)=>(cruid,solarid,dsid,chid)
+
+  auto cru = raw::createCRUEncoderNoPhase<raw::UserLogicFormat, raw::ChargeSumMode>(cruId);
+  uint16_t ts(0);
+
+  uint8_t solarId(0);  // FIXME: get this from digit (deid,padid)=>(....)=>(elinkid,chid)
+  uint16_t elinkId(0); // FIXME: get this from digit  "
+  uint16_t chId(0);    // FIXME: get this from digit  "
+
+  for (int i = 0; i < interactions.size(); i++) {
+
+    auto& col = interactions[i];
+
+    cru->startHeartbeatFrame(col.orbit, col.bc);
+
+    for (auto d : digitsPerInteraction[i]) {
+      cru->addChannelData(solarId, elinkId, chId, {raw::SampaCluster(ts, static_cast<uint16_t>(d.getADC()))});
+    }
+    cru->moveToBuffer(buffer);
+    std::cout << fmt::format("interaction {} buffer size {}\n", i, buffer.size());
+  }
+}
+
+int main()
+{
+  o2::steer::InteractionSampler sampler; // default sampler with default filling scheme, rate of 50 kHz
+
+  constexpr int nofInteractionsPerTimeFrame{1000};
+  constexpr float occupancy = 1E-2;
+
+  std::vector<o2::InteractionTimeRecord> interactions = getCollisions(sampler, nofInteractionsPerTimeFrame); // destination for records
+
+  auto deids = getDetectionElementIds();
+
+  // one vector of digits per interaction
+  // FIXME: should get one such structure per CRU, i.e. per array of detection elements
+  std::vector<std::vector<MCHDigit>> digitsPerInteraction = getDigits(interactions.capacity(), deids, occupancy);
+
+  // encode is supposed to be for 1 CRU only
+  std::vector<uint8_t> buffer;
+  encode(interactions, digitsPerInteraction, buffer);
+
+  std::cout << fmt::format("output buffer is {} MB\n", buffer.size() / 1024 / 1024);
+  return 0;
+}
