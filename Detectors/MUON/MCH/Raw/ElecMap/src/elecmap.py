6e415a5669aa6af36143c7955ad4adcefd7a68b5
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2019-12-17 10:41:27 +0100

    Put all python generation code in a single script.
    No need to bother with a package as it's not a lot of code, and it's then
    easier to use e.g. from FLPs.

--- Detectors/MUON/MCH/Raw/ElecMap/src/elecmap.py
+++ Detectors/MUON/MCH/Raw/ElecMap/src/elecmap.py
@@ -1,14 +1,145 @@
 #!/usr/bin/env python
 
+from oauth2client.service_account import ServiceAccountCredentials
+from string import Template
+from subprocess import call
 import argparse
-import os
-import elecmap.excel
-import elecmap.gencode
-import elecmap.gs
-import pandas as pd
+import gspread
 import numpy as np
+import os
+import pandas as pd
+import sys
+
+def gencode_clang_format(filename):
+    """ Run clang-format on file """
+    lc = ["clang-format","-i",filename] 
+    return call(lc)
+
+def gencode_open_generated(filename):
+    """ Open a new file and add a Copyright on it """
+    out= open(filename,"w")
+    gencode_generated_code(out)
+    return out
+
+def gencode_close_generated(out):
+    """ Format and close """
+    out.close()
+    gencode_clang_format(out.name)
+
+def gencode_generated_code(out):
+    """ Add full O2 Copyright to out"""
+    out.write('''// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+              ///
+              /// GENERATED CODE ! DO NOT EDIT !
+              ///
+              ''')
+
+def gencode_insert_row_in_map(out,row):
+    def insert_in_map(dsid,index):
+        out.write("add(e2d,{},{},{},{},{});\n"
+                  .format(row.de_id,dsid,row.solar_id,row.group_id,index))
+    insert_in_map(row.ds_id_0,0)
+    insert_in_map(row.ds_id_1,1)
+    if row.ds_id_2:
+        insert_in_map(row.ds_id_2,2)
+    if row.ds_id_3:
+        insert_in_map(row.ds_id_3,3)
+    if row.ds_id_4:
+         insert_in_map(row.ds_id_4,4)
+
+def gencode_do(df,chamber):
+    """ Generate code for one chamber
+
+
+    Information from the dataframe df is used to create c++ code that
+    builds a couple of std::map
+    """
+
+    out = gencode_open_generated(chamber + ".cxx")
+
+
+    out.write('''
+              #include <map>
+              #include <cstdint>
+              #include "MCHRawElecMap/DsElecId.h"
+              #include "MCHRawElecMap/DsDetId.h"
+              using namespace o2::mch::raw;
+
+              namespace {
+              void add(std::map<uint16_t,uint32_t>& e2d, int deId, int dsId,
+              uint16_t solarId, uint8_t groupId, uint8_t index) {
+              e2d.emplace(encode(DsElecId(solarId,groupId,index)),encode(DsDetId(deId,dsId)));
+             }
+             }
+              ''')
+
+    out.write("void fillElec2Det{}(std::map<uint16_t,uint32_t>& e2d){{".format(chamber))
+
+    for row in df.itertuples():
+        gencode_insert_row_in_map(out,row)
+
+    out.write("}")
+
+    out.write("void fillSolar2Cru{}(std::map<uint16_t, uint16_t>& s2c){{".format(chamber))
+    out.write("}")
+    gencode_close_generated(out)
+
+def gs_read_sheet(credential_file,workbook,sheet_name):
+    """ Read a Google Spreadsheet 
+
+    """
+
+    scope = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+             credential_file, scope) # Your json file here
+
+    gc = gspread.authorize(credentials)
+
+    wks = gc.open(workbook).worksheet(sheet_name)
+
+    data = wks.get_all_values()
+
+    cols = np.array([0,1,3,4,5,6,7,8,9,10,11,12,13,14])
+    df = pd.DataFrame(np.asarray(data)[:,cols], columns=["cru","fiber","crate","solar",
+                                      "solar_local_id","j","slat",
+                                      "length","de",
+                                      "ds1","ds2","ds3","ds4","ds5"])
+    return df.iloc[3:]
+
+def excel_get_dataframe(filename,sheet):
+    """ Read a dataframe from an excel file """
+
+    f = pd.read_excel(filename,sheet_name=sheet,
+                      names=["cru","fiber","crate","solar",
+                                      "solar_local_id","j","slat",
+                                      "length","de",
+                                      "ds1","ds2","ds3","ds4","ds5"],
+                      usecols="A:N",
+                      na_values=[" "],
+                      na_filter=True)
+    return f
+
+def excel_is_valid_file(parser, arg, sheet):
+    print(arg,sheet)
+    if not os.path.isfile(arg):
+        parser.error("The file %s does not exist!" % arg)
+    else:
+        return excel_get_dataframe(arg,sheet)
+
 
 def _simplify_dataframe(df):
+   """ Do some cleanup on the dataframe """
 
    # remove lines where only the "CRATE #" column is
    # different from NaN
@@ -79,10 +210,10 @@ df = pd.DataFrame()
 
 if args.excel_filename:
     for ifile in args.excel_filename:
-        df = df.append(elecmap.excel.is_valid_file(parser, ifile,args.sheet))
+        df = df.append(excel_is_valid_file(parser, ifile,args.sheet))
 
 if args.gs_name:
-    df = df.append(elecmap.gs.read_sheet(args.credentials,args.gs_name,args.sheet))
+    df = df.append(gs_read_sheet(args.credentials,args.gs_name,args.sheet))
 
 df = _simplify_dataframe(df)
 
