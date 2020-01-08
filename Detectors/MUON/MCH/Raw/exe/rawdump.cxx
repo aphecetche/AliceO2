963b3854526d26fa054dc84ffa0d3d523d359091
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2020-01-08 11:17:34 +0100

    WIP - Compute mean and rms of samples

--- Detectors/MUON/MCH/Raw/exe/rawdump.cxx
+++ Detectors/MUON/MCH/Raw/exe/rawdump.cxx
@@ -29,14 +29,58 @@ extern std::ostream& operator<<(std::ostream&, const o2::header::RAWDataHeaderV4
 using namespace o2::mch::raw;
 using RDHv4 = o2::header::RAWDataHeaderV4;
 
-template <typename FORMAT, typename CHARGESUM, typename RDH>
-int rawdump(std::string input, unsigned int maxNofRDHs, bool showRDHs)
+class DumpOptions
 {
-  if (maxNofRDHs == 0) {
-    maxNofRDHs = std::numeric_limits<unsigned int>::max();
-  }
-  std::cout << __PRETTY_FUNCTION__ << " maxNofRDHs=" << maxNofRDHs << "\n";
+ public:
+  DumpOptions(uint16_t solarOffset, unsigned int maxNofRDHs, bool showRDHs)
+    : mSolarOffset{solarOffset},
+      mMaxNofRDHs{maxNofRDHs == 0 ? std::numeric_limits<unsigned int>::max() : maxNofRDHs},
+      mShowRDHs{showRDHs} {}
 
+  unsigned int maxNofRDHs() const
+  {
+    return mMaxNofRDHs;
+  }
+
+  uint16_t solarOffset() const
+  {
+    return mSolarOffset;
+  }
+
+  bool showRDHs() const
+  {
+    return mShowRDHs;
+  }
+
+ private:
+  uint16_t mSolarOffset;
+  unsigned int mMaxNofRDHs;
+  bool mShowRDHs;
+};
+
+struct Stat {
+  double mean{0};
+  double rms{0};
+  double q{0};
+  int n{0};
+  void incr(int v)
+  {
+    n++;
+    auto newMean = mean + (v - mean) / n;
+    q += (v - newMean) * (v - mean);
+    rms = sqrt(q / n);
+    mean = newMean;
+  }
+};
+
+std::ostream& operator<<(std::ostream& os, const Stat& s)
+{
+  os << fmt::format("MEAN {:7.3f} RMS {:7.3f} NSAMPLES {:5d} ", s.mean, s.rms, s.n);
+  return os;
+}
+template <typename FORMAT, typename CHARGESUM, typename RDH>
+int rawdump(std::string input, DumpOptions opt)
+{
   std::ifstream in(input.c_str(), std::ios::binary);
   if (!in.good()) {
     std::cout << "could not open file " << input << "\n";
@@ -51,25 +95,31 @@ int rawdump(std::string input, unsigned int maxNofRDHs, bool showRDHs)
 
   std::map<std::string, int> uniqueDS;
   std::map<std::string, int> uniqueChannel;
+  std::map<std::string, Stat> statChannel;
 
   memset(&buffer[0], 0, buffer.size());
-  auto channelHandler = [&ndigits, &uniqueDS, &uniqueChannel](DsElecId dsId,
-                                                              uint8_t channel, o2::mch::raw::SampaCluster sc) {
-    std::stringstream ss;
-    ss << dsId;
-    auto s = ss.str();
+  auto channelHandler = [&ndigits, &uniqueDS, &uniqueChannel, &statChannel](DsElecId dsId,
+                                                                            uint8_t channel, o2::mch::raw::SampaCluster sc) {
+    auto s = asString(dsId);
     uniqueDS[s]++;
-    uniqueChannel[fmt::format("{}-CH-{}", s, channel)]++;
+    auto ch = fmt::format("{}-CH-{}", s, channel);
+    uniqueChannel[ch]++;
+    auto& stat = statChannel[ch];
+    for (auto d = 0; d < sc.nofSamples(); d++) {
+      stat.incr(sc.samples[d]);
+    }
     ++ndigits;
   };
 
   size_t nrdhs{0};
   auto rdhHandler = [&](const RDH& rdh) {
     nrdhs++;
-    if (showRDHs) {
+    if (opt.showRDHs()) {
       std::cout << nrdhs << "--" << rdh << "\n";
     }
-    return rdh;
+    auto r = rdh;
+    r.feeId = rdhLinkId(r) + opt.solarOffset();
+    return r;
   };
 
   o2::mch::raw::Decoder decode = o2::mch::raw::createDecoder<FORMAT, CHARGESUM, RDH>(rdhHandler, channelHandler);
@@ -78,7 +128,7 @@ int rawdump(std::string input, unsigned int maxNofRDHs, bool showRDHs)
 
   size_t npages{0};
 
-  while (npages < maxNofRDHs && in.read(reinterpret_cast<char*>(&buffer[0]), pageSize)) {
+  while (npages < opt.maxNofRDHs() && in.read(reinterpret_cast<char*>(&buffer[0]), pageSize)) {
     npages++;
     int n = decode(sbuffer);
   }
@@ -90,7 +140,7 @@ int rawdump(std::string input, unsigned int maxNofRDHs, bool showRDHs)
   }
   std::cout << "--------\n";
   for (auto s : uniqueChannel) {
-    std::cout << s.first << " " << s.second << "\n";
+    std::cout << fmt::format("{:18s}", s.first) << statChannel.at(s.first) << fmt::format("NCLU {:5d}\n", s.second);
   }
   return 0;
 }
@@ -103,9 +153,10 @@ int main(int argc, char* argv[])
   po::variables_map vm;
   po::options_description generic("Generic options");
   unsigned int nrdhs{0};
+  uint16_t solarOffset{0};
   bool showRDHs{false};
-  bool userLogic{false};
-  bool chargeSum{false};
+  bool userLogic{false}; // default format is bareformat...
+  bool chargeSum{false}; //... in sample mode
 
   // clang-format off
   generic.add_options()
@@ -115,6 +166,7 @@ int main(int argc, char* argv[])
       ("showRDHs,s",po::bool_switch(&showRDHs),"show RDHs")
       ("userLogic,u",po::bool_switch(&userLogic),"user logic format")
       ("chargeSum,c",po::bool_switch(&chargeSum),"charge sum format")
+      ("solarOffset,o",po::value<uint16_t>(&solarOffset),"offset to add to linkID to get a solarID")
       ;
   // clang-format on
 
@@ -129,17 +181,19 @@ int main(int argc, char* argv[])
     return 2;
   }
 
+  DumpOptions opt(solarOffset, nrdhs, showRDHs);
+
   if (userLogic) {
     if (chargeSum) {
-      return rawdump<UserLogicFormat, ChargeSumMode, RDHv4>(inputFile, nrdhs, showRDHs);
+      return rawdump<UserLogicFormat, ChargeSumMode, RDHv4>(inputFile, opt);
     } else {
-      return rawdump<UserLogicFormat, SampleMode, RDHv4>(inputFile, nrdhs, showRDHs);
+      return rawdump<UserLogicFormat, SampleMode, RDHv4>(inputFile, opt);
     }
   } else {
     if (chargeSum) {
-      return rawdump<BareFormat, ChargeSumMode, RDHv4>(inputFile, nrdhs, showRDHs);
+      return rawdump<BareFormat, ChargeSumMode, RDHv4>(inputFile, opt);
     } else {
-      return rawdump<BareFormat, SampleMode, RDHv4>(inputFile, nrdhs, showRDHs);
+      return rawdump<BareFormat, SampleMode, RDHv4>(inputFile, opt);
     }
   }
 }
